9f2ad9a9b96dd8f9b96462e1eb5a29d3265e1811
Takumi Kaji <ivezuki3@gmail.com>
2017-05-14 19:31:02 +0900

    Add babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
 
-module.exports = {
+config = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'public', 'js'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {test: /\.(js|jsx)$/, use: 'babel-loader'}
+    ]
   }
 }
+
+module.exports = config;
