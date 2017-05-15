1146fc5c68af45368ac510987b299091110fa710
Takumi Kaji <ivezuki3@gmail.com>
2017-05-16 03:03:10 +0900

    Emphasize current schedule item

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import moment from 'moment';
 import Schedule from './components/schedule'
 import schedules from './data/schedules'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {currentTime: moment()}
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      return this.setState({currentTime: moment()});
+    }, 1000);
+  }
+
   render() {
     return (
-      <Schedule items={schedules} />
+      <Schedule items={schedules} currentTime={this.state.currentTime} />
     );
   }
 }
--- src/components/schedule_item.js
+++ src/components/schedule_item.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
 class ScheduleItem extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {ongoing: false}
+    this.checkOngoing = this.checkOngoing.bind(this)
+  }
+
+  componentDidMount() {
+    this.checkOngoing();
+  }
+
+  componentWillReceiveProps() {
+    this.checkOngoing();
+  }
+
+  checkOngoing() {
+    this.setState({
+      ongoing: this.props.currentTime >= moment(this.props.startAt) && this.props.currentTime < moment(this.props.endAt)
+    });
+  }
+
   render() {
     return (
-      <tr>
+      <tr style={{color: this.state.ongoing ? 'red' : ''}}>
         <td>{this.props.name}</td>
         <td>{this.props.creator}</td>
         <td>{this.props.startAt}</td>
@@ -17,7 +38,8 @@ ScheduleItem.propTypes = {
   name: PropTypes.string.isRequired,
   creator: PropTypes.string.isRequired,
   startAt: PropTypes.string.isRequired,
-  endAt: PropTypes.string.isRequired
+  endAt: PropTypes.string.isRequired,
+  currentTime: PropTypes.object.isRequired
 }
 
 export default ScheduleItem;
